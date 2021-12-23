5ef5fd42689df6470cbb660d96074854d4baf33f
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-23 13:47:42 -0300

    creating backend

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &Copy; 2021
+            <a href="https://www.youtube.com/watch?v=yv9LNdD6rlE" target='_blank'> Gabriel</a>.
+        </strong>
+    </footer>
+)
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,21 @@
+import '../common/template/dependencies'
+import React from 'react'
+
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Routes from './routes'
+
+export default props => {
+    return(
+
+        <div className="wrapper">
+        <Header/>
+        <Sidebar/>
+        <div className='content-wrapper'>
+            <Routes/>
+        </div>
+        <Footer/>
+        </div>
+    )
+}
