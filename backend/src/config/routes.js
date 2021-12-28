39abe55a2eb7c324d1553fb07c7cf99aa21c9bf8
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-28 16:05:17 -0300

    authentication backend

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server){
 
-    const router = express.Router()
+    const protectApi = express.Router()
+    server.use('/api', protectApi)
 
-    server.use('/api', router)
+    protectApi.use(auth)
 
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectApi, '/billingCycles')
 
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
 
+    const AuthService = require('../api/user/authService')
+
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
